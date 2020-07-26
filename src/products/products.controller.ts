2c053b180c2ca06297f61069238981d863d7c3ee
James Hayes <owner@jamesdewitthayes.com>
2020-07-25 17:24:13 -0700

    product dev

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,33 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Product } from './product.model';
 
 // something.com/products
 @Controller('products')
 export class ProductsController {
-    @Post()
-    addProduct(): any{
-        
-    }
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const generatedId = this.productsService.insertProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return { id: generatedId };
+  }
+
+  @Get()
+  getAllProducts(): Product[] {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string): Product {
+    return this.productsService.getSingleProduct(prodId);
+  }
 }
